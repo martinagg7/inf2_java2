5b26fbeb59200a7ca14e4c58944491728ed47354
Martina Garcia <martinagg@myuax.com>
2024-01-23 01:00:45 +0100

    .

---
+++ 03-flujo-control/01-ifs.js
@@ -0,0 +1,13 @@
+/* if (condicio){
+    expresion
+} */
+
+let edad=35
+
+if (edad>25){
+    console.log("usuario mayot edad");
+}else if(edad>13){
+    console.log("acompañado")
+}else{
+    console.log("es menor")
+}
---
+++ 03-flujo-control/03-for.js
@@ -0,0 +1,9 @@
+/*for(inicializacion;comparacion;terminar;):*/
+
+let i =2
+
+for(let i =2;i<10;i++){
+    if (i%2==0){
+        console.log("par",i)
+    }
+}

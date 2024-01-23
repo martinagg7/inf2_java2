27f1a6afee18dbfbd51edcddbed4de5daa997e3a
Martina Garcia <martinagg@myuax.com>
2024-01-23 13:05:09 +0100

    .

---
+++ 04-ejs/07.ejs.js
@@ -0,0 +1,19 @@
+let array1=[{
+    id:1,
+    name:"Nicolas",
+},{
+    id:2,
+    name:"Felipe",
+},{
+    id:3,
+    name:"Chanchito",
+}
+]
+
+function pares(array){
+    let pairs=[]
+    for(let idx=0;idx<array.lenght;id++){
+        let elemento=array[idx]
+        let.pairs.push([elemento.id,elemento])
+    }
+}
